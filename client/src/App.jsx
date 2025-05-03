9a79c47ce334737bdc05b4fac22c1bc79145f5d9
Subek Acharya <subekacharya@gmail.com>
2025-05-02 21:41:00 -0400

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Chat from "./components/chat.jsx";
+
+function App() {
+  return (
+    <>
+      <Chat />
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
+
+// Handling WebSocket connections
+io.on("connection", (socket) => {
+  console.log("A new user has connected", socket.id);
+  
+
+  // Listen for incoming messages from clients
+  socket.on("message", (message) => {
+    // Broadcast the message to all connected clients
+    io.emit("message", message);
+  });
+
+  // Handle disconnections
+  socket.on("disconnect", () => {
+    console.log(socket.id, " disconnected");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
